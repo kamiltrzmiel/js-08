814d1a5bd0b42faf87a65a8ba9e06c51215a0660
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-04-04 15:05:37 +0200

    task 02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoEl = document.getElementById('vimeo-player');
+const player = new Player(videoEl);
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+setInterval(function () {
+  player.getCurrentTime().then(function (time) {
+    localStorage.setItem('videoplayer-current-time', time);
+  });
+}, 1000);
